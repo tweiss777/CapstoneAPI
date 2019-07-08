ca5027940176e2f0617a063eeaac9e455bcb2722
Tal Weiss <Tweiss747@gmail.com>
2019-07-08 21:24:15 +0300

    added authentication header

--- api.py
+++ api.py
@@ -14,17 +14,22 @@ app.config["DEBUG"] = True
     # THIS IS WHAT IS GOING TO BE CALLED IN MY TEST SCRIPT
 @app.route('/api/v1/getJsonData',methods=['POST'])
 def getJobData():
-    # Print statement used for debugging purposes
-    print("request recieved")
+    correctKey = "SSTFZ05I7GYVFVTV9Y4S7DCQOUQ0Y1VHNM6AOS2XHRUUCVG8A7JE15FH8KR3SFWVQ1HH92VD91LE7D7U8U2YTAFL5VAQBM6CMU7V"
+    # Add request headers
+    rheaders = request.headers
     
-    # Parameters to pass to function
-    query = request.json['query']
-    zipcode = request.json['zipcode']
-    resumeStr = request.json['resume']
-    
-    # response from the capstone algorithm
-    results = retrieveApiData(query,zipcode,resumeStr)
-    return jsonify(results)
+    authentication = rheaders.get("X-Api-Key")
+    if authentication == correctKey:
+        # Parameters to pass to function
+        query = request.json['query']
+        zipcode = request.json['zipcode']
+        resumeStr = request.json['resume']
+        
+        # response from the capstone algorithm
+        results = retrieveApiData(query,zipcode,resumeStr)
+        return jsonify(results),200
+    else:
+        return jsonify({"Message":"Access denied,"}),401
 
 # This line below runs the app
 app.run()
