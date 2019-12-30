04c742805ad79b291c8abf90b518746f2b96b3c8
Tal Weiss <Tweiss747@gmail.com>
2019-12-30 15:44:05 +0200

    moved Driver.py main function to a background task using RQ

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import flask
-from flask import request,jsonify
+import redis
+from flask import jsonify, request
+from rq import Queue
+from rq.job import Job
+from rq.registry import FinishedJobRegistry, StartedJobRegistry
 from JobUtils.Driver import main as retrieveApiData
 
 # Create the app
@@ -8,6 +12,16 @@ app = flask.Flask(__name__)
 # Enable debugging
 app.config["DEBUG"] = True
 
+# initialize the redis worker connection
+redisConnection = redis.Redis()
+
+# initialize the queue
+queue = Queue(connection=redisConnection)
+
+
+# started and finished jobs registries (both will listen on the default worker)
+startRegister = StartedJobRegistry('default',connection=redisConnection)
+finishRegister = FinishedJobRegistry('default',connection=redisConnection)
 """This is action that will be called when running the api
     Returns json data"""
     # IMPLEMENTATION INCOMPLETE
@@ -18,6 +32,7 @@ def getJobData():
     # Add request headers
     rheaders = request.headers
     
+    # api key check
     authentication = rheaders.get("X-Api-Key")
     if authentication == correctKey:
         # Parameters to pass to function
@@ -26,10 +41,39 @@ def getJobData():
         resumeStr = request.json['resume']
         
         # response from the capstone algorithm
-        results = retrieveApiData(query,zipcode,resumeStr)
-        return jsonify(results),200
-    else:
-        return jsonify({"Message":"Access denied,"}),401
+        # results = retrieveApiData(query,zipcode,resumeStr)
+        
+        # store process to be run by the queue
+        process = queue.enqueue(retrieveApiData,args=(query,zipcode,resumeStr))
+        
+        # return the process id
+        return jsonify({"message": "processing the job this may take some time...",
+                        "process_id": process.id}),200
+    return jsonify({"Message":"Access denied,"}),401
+
+
+# function that pings the server for the response
+# this should be called by the client
+@app.route("/api/v1/checkresults",methods=["GET"])
+def returnResults():
+    # retrieve the process id from the url
+    process_id = request.args.get('pid')
+    # if pid is empty or invlaid
+    if process_id is None:
+        return jsonify({"message":"invalid params"}),400
+
+    elif process_id not in startRegister.get_job_ids() and process_id not in finishRegister.get_job_ids():
+        return jsonify({"message": "invalid pid"}), 400
+
+    # fetch the job handled by redis queue
+    currentJob = Job.fetch(process_id,redisConnection)
+    
+    # return a status 202 if the job is still processing 
+    if currentJob.result is None:
+        return jsonify({"message":"job is still processing...."}),202
+
+    # return a 200 status along with the results
+    return jsonify(currentJob.result),200
 
 # This line below runs the app
 if __name__ == "__main__":
